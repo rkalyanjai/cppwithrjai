494cb6d55ee9e4b1a72f798d22ce52d3bc5a60ea
rkalyanjai <81039229+rkalyanjai@users.noreply.github.com>
2021-03-24 02:21:56 +0530

    Create add two numbers with test cases.cpp

---
+++ add two numbers with test cases.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int T,N,a,b;
+    cin>>T;
+    for(int i=1;i<=T;i++)
+    {
+        cin>>a>>b;
+        N=a+b;
+        cout<<N<<endl;
+    }
+    return 0;
+}
